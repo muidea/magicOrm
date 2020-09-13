55b9cd7b4ab726f8484ad7ddbba7e0314427cf52
muidea <rangh@foxmail.com>
2020-09-13 18:16:02 +0800

    update code

--- util/const.go
+++ util/const.go
@@ -60,41 +60,8 @@ func IsSliceType(typeValue int) bool {
 	return typeValue == TypeSliceField
 }
 
-// GetBasicTypeInitValue GetBasicTypeInitValue
-func GetBasicTypeInitValue(typeValue int) (ret interface{}, err error) {
-	switch typeValue {
-	case TypeBooleanField,
-		TypeBitField, TypeSmallIntegerField, TypeIntegerField, TypeInteger32Field, TypeBigIntegerField:
-		val := int64(0)
-		ret = &val
-		break
-	case TypePositiveBitField, TypePositiveSmallIntegerField, TypePositiveIntegerField, TypePositiveInteger32Field, TypePositiveBigIntegerField:
-		val := uint64(0)
-		ret = &val
-		break
-	case TypeStringField, TypeDateTimeField:
-		val := ""
-		ret = &val
-		break
-	case TypeFloatField, TypeDoubleField:
-		val := 0.00
-		ret = &val
-		break
-	case TypeStructField:
-		val := 0
-		ret = &val
-	case TypeSliceField:
-		val := ""
-		ret = &val
-	default:
-		err = fmt.Errorf("no support fileType, %d", typeValue)
-	}
-
-	return
-}
-
-// GetTypeValueEnum return field type as type constant from reflect.Value
-func GetTypeValueEnum(val reflect.Type) (ret int, err error) {
+// GetTypeEnum return field type as type constant from reflect.Value
+func GetTypeEnum(val reflect.Type) (ret int, err error) {
 	switch val.Kind() {
 	case reflect.Int8:
 		ret = TypeBitField
@@ -140,25 +107,6 @@ func GetTypeValueEnum(val reflect.Type) (ret int, err error) {
 	return
 }
 
-// GetSliceRawTypeEnum get slice rawType
-func GetSliceRawTypeEnum(sliceType reflect.Type) (ret int, err error) {
-	if sliceType.Kind() != reflect.Slice {
-		err = fmt.Errorf("illegal type, not slice. typeVal:%s", sliceType.Kind().String())
-		return
-	}
-
-	rawType := sliceType.Elem()
-	if rawType.Kind() == reflect.Ptr {
-		rawType = rawType.Elem()
-	}
-	ret, err = GetTypeValueEnum(rawType)
-	if err != nil {
-		return
-	}
-
-	return
-}
-
 // IsNil check value if nil
 func IsNil(val reflect.Value) (ret bool) {
 	if val.Kind() == reflect.Ptr {
@@ -181,40 +129,16 @@ func IsNil(val reflect.Value) (ret bool) {
 	return
 }
 
-//IsSame check if same
-func IsSame(firstVal, secondVal reflect.Value) (ret bool, err error) {
-	ret = firstVal.Type().String() == secondVal.Type().String()
-	if !ret {
-		return
-	}
-
-	firstIsNil := IsNil(firstVal)
-	secondIsNul := IsNil(secondVal)
-	if firstIsNil != secondIsNul {
+//isSameStruct check if same
+func isSameStruct(firstVal, secondVal reflect.Value) (ret bool, err error) {
+	firstNum := firstVal.NumField()
+	secondNum := secondVal.NumField()
+	if firstNum != secondNum {
 		ret = false
 		return
 	}
-	if firstIsNil {
-		ret = true
-		return
-	}
 
-	firstVal = reflect.Indirect(firstVal)
-	secondVal = reflect.Indirect(secondVal)
-	typeVal, typeErr := GetTypeValueEnum(firstVal.Type())
-	if typeErr != nil {
-		ret = false
-		err = typeErr
-		return
-	}
-	if typeVal != TypeStructField {
-		ret = false
-		err = fmt.Errorf("illegal value, type:%s", firstVal.Type().String())
-		return
-	}
-
-	numField := firstVal.NumField()
-	for idx := 0; idx < numField; idx++ {
+	for idx := 0; idx < firstNum; idx++ {
 		firstField := firstVal.Field(idx)
 		secondField := secondVal.Field(idx)
 		ret, err = IsSameVal(firstField, secondField)
@@ -224,6 +148,7 @@ func IsSame(firstVal, secondVal reflect.Value) (ret bool, err error) {
 		}
 	}
 
+	ret = true
 	return
 }
 
@@ -246,7 +171,7 @@ func IsSameVal(firstVal, secondVal reflect.Value) (ret bool, err error) {
 	}
 	firstVal = reflect.Indirect(firstVal)
 	secondVal = reflect.Indirect(secondVal)
-	typeVal, typeErr := GetTypeValueEnum(firstVal.Type())
+	typeVal, typeErr := GetTypeEnum(firstVal.Type())
 	if typeErr != nil {
 		err = typeErr
 		ret = false
@@ -254,12 +179,10 @@ func IsSameVal(firstVal, secondVal reflect.Value) (ret bool, err error) {
 	}
 
 	if IsStructType(typeVal) {
-		ret, err = IsSame(firstVal, secondVal)
+		ret, err = isSameStruct(firstVal, secondVal)
 		return
 	}
 
-	firstVal = reflect.Indirect(firstVal)
-	secondVal = reflect.Indirect(secondVal)
 	if IsBasicType(typeVal) {
 		switch typeVal {
 		case TypeBooleanField:
