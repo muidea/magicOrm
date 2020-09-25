b3063f92e210988114876a91e4ffd869b8aa3187
muidea <rangh@foxmail.com>
2020-09-25 17:31:55 +0800

    update code

--- test/base.go
+++ test/base.go
@@ -20,6 +20,41 @@ type Simple struct {
 	Flag      bool      `orm:"flag"`
 }
 
+func (l *Simple) IsSame(r *Simple) bool {
+	if l.ID != r.ID {
+		return false
+	}
+	if l.I8 != r.I8 {
+		return false
+	}
+	if l.I16 != r.I16 {
+		return false
+	}
+	if l.I32 != r.I32 {
+		return false
+	}
+	if l.I64 != r.I64 {
+		return false
+	}
+	if l.Name != r.Name {
+		return false
+	}
+	if l.Value != r.Value {
+		return false
+	}
+	if l.F64 != r.F64 {
+		return false
+	}
+	if l.TimeStamp.Sub(r.TimeStamp) != 0 {
+		return false
+	}
+	if l.Flag != r.Flag {
+		return false
+	}
+
+	return true
+}
+
 type Reference struct {
 	ID          int        `orm:"id key auto"`
 	Name        string     `orm:"name"`
@@ -36,6 +71,100 @@ type Reference struct {
 	PtrStrArray *[]*string `orm:"ptrStrArray"`
 }
 
+func (l *Reference) IsSame(r *Reference) bool {
+	if l.ID != r.ID {
+		return false
+	}
+	if l.Name != r.Name {
+		return false
+	}
+	if l.F64 != r.F64 {
+		return false
+	}
+	if l.FValue != nil {
+		if r.FValue == nil {
+			return false
+		}
+		if *l.FValue != *r.FValue {
+			return false
+		}
+	}
+	if l.FValue == nil {
+		if r.FValue != nil {
+			return false
+		}
+	}
+	if l.TimeStamp != nil {
+		if r.TimeStamp == nil {
+			return false
+		}
+		if l.TimeStamp.Sub(*r.TimeStamp) != 0 {
+			return false
+		}
+	}
+	if l.TimeStamp == nil {
+		if r.TimeStamp != nil {
+			return false
+		}
+	}
+	if l.Flag != nil {
+		if r.Flag == nil {
+			return false
+		}
+		if *l.Flag != *r.Flag {
+			return false
+		}
+	}
+	if l.Flag == nil {
+		if r.Flag != nil {
+			return false
+		}
+	}
+	if len(l.IArray) != len(r.IArray) {
+		return false
+	}
+	if len(l.FArray) != len(r.FArray) {
+		return false
+	}
+	if len(l.StrArray) != len(r.StrArray) {
+		return false
+	}
+	if len(l.BArray) != len(r.BArray) {
+		return false
+	}
+	if l.PtrArray != nil {
+		if r.PtrArray == nil {
+			return false
+		}
+		if len(*l.PtrArray) != len(*r.PtrArray) {
+			return false
+		}
+	}
+	if l.PtrArray == nil {
+		if r.PtrArray != nil {
+			return false
+		}
+	}
+	if len(l.StrPtrArray) != len(r.StrPtrArray) {
+		return false
+	}
+	if l.PtrStrArray != nil {
+		if r.PtrStrArray == nil {
+			return false
+		}
+		if len(*l.PtrStrArray) != len(*r.PtrStrArray) {
+			return false
+		}
+	}
+	if l.PtrStrArray == nil {
+		if r.PtrStrArray != nil {
+			return false
+		}
+	}
+
+	return true
+}
+
 type Compose struct {
 	ID             int           `orm:"id key auto"`
 	Name           string        `orm:"name"`
@@ -50,6 +179,93 @@ type Compose struct {
 	RefPtrArray    []*Reference  `orm:"refPtrArray"`
 	PtrRefArray    *[]*Reference `orm:"ptrRefArray"`
 	PtrCompose     *Compose      `orm:"ptrCompose"`
+}
+
+func (l *Compose) IsSame(r *Compose) bool {
+	if l.ID != r.ID {
+		return false
+	}
+	if l.Name != r.Name {
+		return false
+	}
+	if !l.Simple.IsSame(&r.Simple) {
+		return false
+	}
+	if l.PtrSimple != nil {
+		if r.PtrSimple == nil {
+			return false
+		}
+	}
+	if l.PtrSimple == nil {
+		if r.PtrSimple != nil {
+			return false
+		}
+	}
+	if len(l.SimpleArray) != len(r.SimpleArray) {
+		return false
+	}
+	if len(l.SimplePtrArray) != len(r.SimplePtrArray) {
+		return false
+	}
+	if l.PtrSimpleArray != nil {
+		if r.PtrSimpleArray == nil {
+			return false
+		}
+		if len(*l.PtrSimpleArray) != len(*r.PtrSimpleArray) {
+			return false
+		}
+	}
+	if l.PtrSimpleArray == nil {
+		if r.PtrSimpleArray != nil {
+			return false
+		}
+	}
+	if !l.Reference.IsSame(&r.Reference) {
+		return false
+	}
+	if l.PtrReference != nil {
+		if r.PtrReference == nil {
+			return false
+		}
+		if !l.PtrReference.IsSame(r.PtrReference) {
+			return false
+		}
+	}
+	if l.PtrReference == nil {
+		if r.PtrReference != nil {
+			return false
+		}
+	}
+	if len(l.RefArray) != len(r.RefArray) {
+		return false
+	}
+	if len(l.RefPtrArray) != len(r.RefPtrArray) {
+		return false
+	}
+	if l.PtrRefArray != nil {
+		if r.PtrRefArray == nil {
+			return false
+		}
+		if len(*l.PtrRefArray) != len(*r.PtrRefArray) {
+			return false
+		}
+	}
+	if l.PtrRefArray == nil {
+		if r.PtrRefArray != nil {
+			return false
+		}
+	}
+	if l.PtrCompose != nil {
+		if r.PtrCompose == nil {
+			return false
+		}
+
+		if !l.PtrCompose.IsSame(r.PtrCompose) {
+			return false
+		}
+	}
+
+	return true
 }
 
 func registerModel(orm orm.Orm, objList []interface{}) (err error) {
--- test/base_local_test.go
+++ test/base_local_test.go
@@ -3,6 +3,7 @@ package test
 import (
 	orm "github.com/muidea/magicOrm"
 	"testing"
+	"time"
 )
 
 func TestLocalSimple(t *testing.T) {
@@ -19,4 +20,26 @@ func TestLocalSimple(t *testing.T) {
 	objList := []interface{}{&Simple{}}
 	registerModel(o1, objList)
 
+	now, _ := time.ParseInLocation("2006-01-02 15:04:05:0000", "2018-01-02 15:04:05:0000", time.Local)
+	s1 := &Simple{I8: 12, I16: 23, I32: 34, I64: 45, Name: "test code", Value: 12.345, F64: 23.456, TimeStamp: now, Flag: true}
+
+	err = o1.Insert(s1, "test")
+	if err != nil {
+		t.Errorf("insert simple failed, err:%s", err.Error())
+		return
+	}
+
+	s1.Name = "hello"
+	err = o1.Update(s1, "test")
+	if err != nil {
+		t.Errorf("update simple failed, err:%s", err.Error())
+		return
+	}
+
+	s2 := &Simple{ID: s1.ID}
+	err = o1.Query(s2, "test")
+	if err != nil {
+		t.Errorf("query simple failed, err:%s", err.Error())
+		return
+	}
 }
