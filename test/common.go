84c9aef585570da88ecf5daa420b567825c780d0
muidea <rangh@foxmail.com>
2020-09-25 13:54:06 +0800

    update code

--- test/common.go
+++ test/common.go
@@ -10,18 +10,19 @@ import (
 
 // Unit 单元信息
 type Unit struct {
-	//ID 唯一标示单元
-	ID  int    `json:"id" orm:"id key auto"`
-	I8  int8   `orm:"i8"`
-	I16 int16  `orm:"i16"`
-	I32 int32  `orm:"i32"`
-	I64 uint64 `orm:"i64"`
-	// Name 名称
-	Name      string    `json:"name" orm:"name"`
-	Value     float32   `json:"value" orm:"value"`
+	ID        int       `orm:"id key auto"`
+	I8        int8      `orm:"i8"`
+	I16       int16     `orm:"i16"`
+	I32       int32     `orm:"i32"`
+	I64       uint64    `orm:"i64"`
+	Name      string    `orm:"name"`
+	Value     float32   `orm:"value"`
 	F64       float64   `orm:"f64"`
-	TimeStamp time.Time `json:"timeStamp" orm:"ts"`
+	TimeStamp time.Time `orm:"ts"`
 	Flag      bool      `orm:"flag"`
+	IArray    []int     `orm:"iArray"`
+	FArray    []float32 `orm:"fArray"`
+	StrArray  []string  `orm:"strArray"`
 }
 
 // ExtUnit ExtUnit
---
+++ test/def.go
@@ -0,0 +1,48 @@
+package test
+
+import "time"
+
+type Simple struct {
+	ID        int       `orm:"id key auto"`
+	I8        int8      `orm:"i8"`
+	I16       int16     `orm:"i16"`
+	I32       int32     `orm:"i32"`
+	I64       uint64    `orm:"i64"`
+	Name      string    `orm:"name"`
+	Value     float32   `orm:"value"`
+	F64       float64   `orm:"f64"`
+	TimeStamp time.Time `orm:"ts"`
+	Flag      bool      `orm:"flag"`
+}
+
+type Reference struct {
+	ID          int        `orm:"id key auto"`
+	Name        string     `orm:"name"`
+	FValue      *float32   `orm:"value"`
+	F64         float64    `orm:"f64"`
+	TimeStamp   *time.Time `orm:"ts"`
+	Flag        *bool      `orm:"flag"`
+	IArray      []int      `orm:"iArray"`
+	FArray      []float32  `orm:"fArray"`
+	StrArray    []string   `orm:"strArray"`
+	BArray      []bool     `orm:"bArray"`
+	PtrArray    *[]string  `orm:"ptrArray"`
+	StrPtrArray []*string  `orm:"strPtrArray"`
+	PtrStrArray *[]*string `orm:"ptrStrArray"`
+}
+
+type Compose struct {
+	ID             int           `orm:"id key auto"`
+	Name           string        `orm:"name"`
+	Simple         Simple        `orm:"simple"`
+	PtrSimple      *Simple       `orm:"ptrSimple"`
+	SimpleArray    []Simple      `orm:"simpleArray"`
+	SimplePtrArray []*Simple     `orm:"simplePtrArray"`
+	PtrSimpleArray *[]Simple     `orm:"ptrSimpleArray"`
+	Reference      Reference     `orm:"reference"`
+	PtrReference   *Reference    `orm:"ptrReference"`
+	RefArray       []Reference   `orm:"refArray"`
+	RefPtrArray    []*Reference  `orm:"refPtrArray"`
+	PtrRefArray    *[]*Reference `orm:"ptrRefArray"`
+	PtrCompose     *Compose      `orm:"ptrCompose"`
+}
