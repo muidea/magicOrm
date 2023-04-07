f356d5a2b9e5b4e9fac3fb82868d7530917e9b8e
muidea <rangh@foxmail.com>
2023-04-07 23:25:08 +0800

    update code

--- database/mysql/demo/main.go
+++ database/mysql/demo/main.go
@@ -1,18 +1,19 @@
-package mysql
+package main
 
 import (
+	"flag"
+	log "github.com/cihub/seelog"
 	"math/rand"
 	"sync"
-	"testing"
 	"time"
 
-	log "github.com/cihub/seelog"
+	"github.com/muidea/magicOrm/database/mysql"
 )
 
 const (
-	onceDML   = 0
-	onceDDL   = 1
-	monkeyDDL = 2
+	onceDML   = 1
+	onceDDL   = 2
+	monkeyDDL = 3
 )
 
 var databaseServer = "localhost:3306"
@@ -22,14 +23,26 @@ var databasePassword = "rootkit"
 var threadSize = 20
 var itemSize = 10000000
 var mode = 0
-
 var finishFlag = false
 
-type funcPtr func(executor *Executor) error
+type funcPtr func(executor *mysql.Executor) error
 
-func TestNewPool(t *testing.T) {
-	pool := NewPool()
-	config := NewConfig(databaseServer, databaseName, databaseUsername, databasePassword)
+func main() {
+	flag.StringVar(&databaseServer, "Server", databaseServer, "database server address")
+	flag.StringVar(&databaseName, "Database", databaseName, "database name")
+	flag.StringVar(&databaseUsername, "Account", databaseUsername, "database account")
+	flag.StringVar(&databasePassword, "Password", databasePassword, "database password")
+	flag.IntVar(&threadSize, "ThreadSize", threadSize, "database access thread size")
+	flag.IntVar(&itemSize, "ItemSize", itemSize, "database insert item size")
+	flag.IntVar(&mode, "Mode", mode, "database access mode, 1=onceDML,2=onceDDL,3=monkeyDDL")
+	flag.Parse()
+	if mode == 0 {
+		flag.PrintDefaults()
+		return
+	}
+
+	pool := mysql.NewPool()
+	config := mysql.NewConfig(databaseServer, databaseName, databaseUsername, databasePassword)
 	pool.Initialize(50, config)
 	defer pool.Uninitialized()
 
@@ -59,7 +72,7 @@ func TestNewPool(t *testing.T) {
 	wg.Wait()
 }
 
-func testDML(wg *sync.WaitGroup, pool *Pool) {
+func testDML(wg *sync.WaitGroup, pool *mysql.Pool) {
 	finishFlag = false
 	wg.Add(1)
 	pickExecutor(pool, wg, dropSchema)
@@ -74,7 +87,7 @@ func testDML(wg *sync.WaitGroup, pool *Pool) {
 	finishFlag = true
 }
 
-func testDDL(wg *sync.WaitGroup, pool *Pool) {
+func testDDL(wg *sync.WaitGroup, pool *mysql.Pool) {
 	wg.Add(1)
 	pickExecutor(pool, wg, alterSchemaAdd)
 
@@ -88,7 +101,7 @@ func testDDL(wg *sync.WaitGroup, pool *Pool) {
 	pickExecutor(pool, wg, alterSchemaOlnDDLDrop)
 }
 
-func testMonkey(wg *sync.WaitGroup, pool *Pool) {
+func testMonkey(wg *sync.WaitGroup, pool *mysql.Pool) {
 	testDML(wg, pool)
 
 	go func() {
@@ -99,7 +112,7 @@ func testMonkey(wg *sync.WaitGroup, pool *Pool) {
 	}()
 }
 
-func randomDDL(wg *sync.WaitGroup, pool *Pool) {
+func randomDDL(wg *sync.WaitGroup, pool *mysql.Pool) {
 	testDDL(wg, pool)
 	wg.Add(1)
 	pickExecutor(pool, wg, createSchemaDDL)
@@ -108,7 +121,7 @@ func randomDDL(wg *sync.WaitGroup, pool *Pool) {
 	pickExecutor(pool, wg, dropSchemaDDL)
 }
 
-func pickExecutor(pool *Pool, wg *sync.WaitGroup, fPtr funcPtr) {
+func pickExecutor(pool *mysql.Pool, wg *sync.WaitGroup, fPtr funcPtr) {
 	executorPtr, executorErr := pool.FetchOut()
 	if executorErr != nil {
 		return
@@ -126,24 +139,24 @@ func pickExecutor(pool *Pool, wg *sync.WaitGroup, fPtr funcPtr) {
 	wg.Done()
 }
 
-func createSchema(executor *Executor) (err error) {
+func createSchema(executor *mysql.Executor) (err error) {
 	sql := "CREATE TABLE `Unit` (\n\t`id` INT NOT NULL AUTO_INCREMENT,\n\t`i8` TINYINT NOT NULL ,\n\t`i16` SMALLINT NOT NULL ,\n\t`i32` INT NOT NULL ,\n\t`i64` BIGINT NOT NULL ,\n\t`name` TEXT NOT NULL ,\n\t`value` FLOAT NOT NULL ,\n\t`f64` DOUBLE NOT NULL ,\n\t`ts` DATETIME NOT NULL ,\n\t`flag` TINYINT NOT NULL ,\n\t`iArray` TEXT NOT NULL ,\n\t`fArray` TEXT NOT NULL ,\n\t`strArray` TEXT NOT NULL ,\n\tPRIMARY KEY (`id`)\n)"
 	_, _, err = executor.Execute(sql)
 	return
 }
 
-func dropSchema(executor *Executor) (err error) {
+func dropSchema(executor *mysql.Executor) (err error) {
 	sql := "DROP TABLE `Unit`"
 	_, _, err = executor.Execute(sql)
 	return
 }
 
-func checkSchema(executor *Executor) (ret bool, err error) {
+func checkSchema(executor *mysql.Executor) (ret bool, err error) {
 	ret, err = executor.CheckTableExist(`Unit`)
 	return
 }
 
-func insertValue(executor *Executor) (err error) {
+func insertValue(executor *mysql.Executor) (err error) {
 	sql := "INSERT INTO `Unit` (`i8`,`i16`,`i32`,`i64`,`name`,`value`,`f64`,`ts`,`flag`,`iArray`,`fArray`,`strArray`) VALUES (8,1600,323200,78962222222,'Hello world',12.345600128173828,12.45678,'2018-01-02 15:04:05',1,'12','12.34','abcdef')"
 	idx := 0
 	for idx < itemSize/threadSize {
@@ -153,38 +166,38 @@ func insertValue(executor *Executor) (err error) {
 	return
 }
 
-func alterSchemaAdd(executor *Executor) (err error) {
+func alterSchemaAdd(executor *mysql.Executor) (err error) {
 	sql := "ALTER TABLE `Unit` ADD dVal DATE"
 	_, _, err = executor.Execute(sql)
 	return
 }
 
-func alterSchemaDrop(executor *Executor) (err error) {
+func alterSchemaDrop(executor *mysql.Executor) (err error) {
 	sql := "ALTER TABLE `Unit` DROP dVal"
 	_, _, err = executor.Execute(sql)
 	return
 }
 
-func alterSchemaOlnDDLAdd(executor *Executor) (err error) {
+func alterSchemaOlnDDLAdd(executor *mysql.Executor) (err error) {
 	sql := "ALTER TABLE `Unit` ADD dVal2 DATE, ALGORITHM=DEFAULT, LOCK=NONE"
 	_, _, err = executor.Execute(sql)
 	return
 }
 
-func alterSchemaOlnDDLDrop(executor *Executor) (err error) {
+func alterSchemaOlnDDLDrop(executor *mysql.Executor) (err error) {
 	sql := "ALTER TABLE `Unit` DROP dVal2, ALGORITHM=DEFAULT, LOCK=NONE"
 	_, _, err = executor.Execute(sql)
 	return
 }
 
-func createSchemaDDL(executor *Executor) (err error) {
-	sql := "CREATE TABLE `Unit002` (\n\t`id` INT NOT NULL AUTO_INCREMENT,\n\t`i8` TINYINT NOT NULL ,\n\t`i16` SMALLINT NOT NULL ,\n\t`i32` INT NOT NULL ,\n\t`i64` BIGINT NOT NULL ,\n\t`name` TEXT NOT NULL ,\n\t`value` FLOAT NOT NULL ,\n\t`f64` DOUBLE NOT NULL ,\n\t`ts` DATETIME NOT NULL ,\n\t`flag` TINYINT NOT NULL ,\n\t`iArray` TEXT NOT NULL ,\n\t`fArray` TEXT NOT NULL ,\n\t`strArray` TEXT NOT NULL ,\n\tPRIMARY KEY (`id`)\n)"
+func createSchemaDDL(executor *mysql.Executor) (err error) {
+	sql := "CREATE TABLE `Unit002` (\n\t`id` INT NOT NULL AUTO_INCREMENT,\n\t`i8` TINYINT NOT NULL ,\n\t`i16` SMALLINT NOT NULL ,\n\t`i32` INT NOT NULL ,\n\t`i64` BIGINT NOT NULL ,\n\t`name` TEXT NOT NULL ,\n\t`value` FLOAT NOT NULL ,\n\t`f64` DOUBLE NOT NULL ,\n\t`ts` DATETIME NOT NULL ,\n\t`flag` TINYINT NOT NULL ,\n\t`iArray` TEXT NOT NULL ,\n\t`fArray` TEXT NOT NULL ,\n\t`strArray` TEXT NOT NULL ,\n\tPRIMARY KEY (`id`)\n), ALGORITHM=DEFAULT, LOCK=NONE"
 	_, _, err = executor.Execute(sql)
 	return
 }
 
-func dropSchemaDDL(executor *Executor) (err error) {
-	sql := "DROP TABLE `Unit002`"
+func dropSchemaDDL(executor *mysql.Executor) (err error) {
+	sql := "DROP TABLE `Unit002`, ALGORITHM=DEFAULT, LOCK=NONE"
 	_, _, err = executor.Execute(sql)
 	return
 }
