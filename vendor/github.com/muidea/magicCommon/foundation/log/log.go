5b9dba7cc22feb582be0e39e9b5333cc144a879d
muidea <rangh@foxmail.com>
2023-09-21 19:30:52 +0800

    update code

---
+++ vendor/github.com/muidea/magicCommon/foundation/log/log.go
@@ -0,0 +1,87 @@
+package log
+
+import (
+	log "github.com/cihub/seelog"
+)
+
+func init() {
+	logger, _ := log.LoggerFromConfigAsBytes([]byte(logConfig))
+	logger.SetAdditionalStackDepth(1)
+	log.ReplaceLogger(logger)
+}
+
+var logConfig = `<?xml version="1.0" encoding="utf-8"?>
+<seelog levels="trace,debug,info,warn,error,critical" type="sync"> 
+  <outputs formatid="main"> 
+    <!-- 对控制台输出的Log按级别分别用颜色显示。6种日志级别我仅分了三组颜色，如果想每个级别都用不同颜色则需要简单修改即可 -->  
+    <filter levels="trace,debug,info"> 
+      <console formatid="colored-default"/>  
+    </filter>  
+    <filter levels="warn"> 
+      <console formatid="colored-warn"/>  
+    </filter>  
+    <filter levels="error,critical"> 
+      <console formatid="colored-error"/>  
+    </filter> 
+  </outputs>  
+  <formats> 
+    <format id="colored-default" format="%EscM(38)%Date %Time [%LEV] %RelFile:%Line | %Msg%n%EscM(0)"/>  
+    <format id="colored-warn" format="%EscM(33)%Date %Time [%LEV] %RelFile:%Line | %Msg%n%EscM(0)"/>  
+    <format id="colored-error" format="%EscM(31)%Date %Time [%LEV] %RelFile:%Line | %Msg%n%EscM(0)"/>  
+    <format id="main" format="%Date %Time [%LEV] %RelFile:%Line | %Msg%n"/> 
+  </formats> 
+</seelog>`
+
+func Tracef(format string, params ...interface{}) {
+	if len(params) > 0 {
+		log.Tracef(format, params)
+		return
+	}
+
+	log.Trace(format)
+}
+
+func Debugf(format string, params ...interface{}) {
+	if len(params) > 0 {
+		log.Debugf(format, params)
+		return
+	}
+
+	log.Debug(format)
+}
+
+func Infof(format string, params ...interface{}) {
+	if len(params) > 0 {
+		log.Infof(format, params)
+		return
+	}
+
+	log.Info(format)
+}
+
+func Warnf(format string, params ...interface{}) {
+	if len(params) > 0 {
+		log.Warnf(format, params)
+		return
+	}
+
+	log.Warn(params)
+}
+
+func Errorf(format string, params ...interface{}) {
+	if len(params) > 0 {
+		log.Errorf(format, params)
+		return
+	}
+
+	log.Error(format)
+}
+
+func Criticalf(format string, params ...interface{}) {
+	if len(params) > 0 {
+		log.Criticalf(format, params...)
+		return
+	}
+
+	log.Critical(format)
+}
---
+++ vendor/github.com/muidea/magicCommon/foundation/util/filter.go
@@ -0,0 +1,431 @@
+package util
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
+	"path"
+	"strings"
+)
+
+// Filter value filter
+type Filter interface {
+	Filter(val interface{}) bool
+}
+
+// ContentFilter 过滤器
+type ContentFilter struct {
+	PaginationPtr *Pagination `json:"pagination"`
+	ParamItems    *ParamItems `json:"params"`
+}
+
+// NewFilter new filter
+func NewFilter(name, pkgPath string) *ContentFilter {
+	return &ContentFilter{PaginationPtr: nil, ParamItems: &ParamItems{Name: name, PkgPath: pkgPath, Items: map[string]string{}}}
+}
+
+// Decode 内容过滤器
+func (s *ContentFilter) Decode(request *http.Request) bool {
+	pageFilter := &Pagination{}
+	if pageFilter.Decode(request) {
+		s.PaginationPtr = pageFilter
+	}
+
+	contentFilter := &ParamItems{}
+	if contentFilter.Decode(request) {
+		s.ParamItems = contentFilter
+	}
+
+	return s.PaginationPtr != nil || s.ParamItems != nil
+}
+
+// Encode encode filter
+func (s *ContentFilter) Encode(vals url.Values) url.Values {
+	if s.PaginationPtr != nil {
+		vals = s.PaginationPtr.Encode(vals)
+	}
+	if s.ParamItems != nil {
+		vals = s.ParamItems.Encode(vals)
+	}
+
+	return vals
+}
+
+func (s *ContentFilter) GetName() string {
+	if s.ParamItems != nil {
+		return s.ParamItems.Name
+	}
+
+	return ""
+}
+
+func (s *ContentFilter) GetPkgPath() string {
+	if s.ParamItems != nil {
+		return s.ParamItems.PkgPath
+	}
+
+	return ""
+}
+
+func (s *ContentFilter) GetPkeKey() string {
+	if s.ParamItems != nil {
+		return path.Join(s.ParamItems.PkgPath, s.ParamItems.Name)
+	}
+
+	return ""
+}
+
+func (s *ContentFilter) Get(key string) (val string, ok bool) {
+	if s.ParamItems != nil {
+		val, ok = s.ParamItems.Items[key]
+		val = strings.Trim(val, "\"")
+		return
+	}
+
+	return
+}
+
+func (s *ContentFilter) Set(key string, value interface{}) {
+	if s.ParamItems != nil {
+		s.ParamItems.Items[key] = MarshalString(value)
+	}
+}
+
+func (s *ContentFilter) Equal(key string, value interface{}) {
+	if s.ParamItems != nil {
+		s.ParamItems.Items[key] = fmt.Sprintf("%v|=", MarshalString(value))
+	}
+}
+
+func (s *ContentFilter) NotEqual(key string, value interface{}) {
+	if s.ParamItems != nil {
+		s.ParamItems.Items[key] = fmt.Sprintf("%v|!=", MarshalString(value))
+	}
+}
+
+func (s *ContentFilter) Below(key string, value interface{}) {
+	if s.ParamItems != nil {
+		s.ParamItems.Items[key] = fmt.Sprintf("%v|<", MarshalString(value))
+	}
+}
+
+func (s *ContentFilter) Above(key string, value interface{}) {
+	if s.ParamItems != nil {
+		s.ParamItems.Items[key] = fmt.Sprintf("%v|>", MarshalString(value))
+	}
+}
+
+func (s *ContentFilter) In(key string, value interface{}) {
+	if s.ParamItems != nil {
+		s.ParamItems.Items[key] = fmt.Sprintf("%v|in", MarshalString(value))
+	}
+}
+
+func (s *ContentFilter) NotIn(key string, value interface{}) {
+	if s.ParamItems != nil {
+		s.ParamItems.Items[key] = fmt.Sprintf("%v|!in", MarshalString(value))
+	}
+}
+
+func (s *ContentFilter) Like(key string, value interface{}) {
+	if s.ParamItems != nil {
+		s.ParamItems.Items[key] = fmt.Sprintf("%v|like", value)
+	}
+}
+
+func (s *ContentFilter) Remove(key string) {
+	if s.ParamItems != nil {
+		delete(s.ParamItems.Items, key)
+	}
+}
+
+func (s *ContentFilter) Pagination(ptr *Pagination) {
+	s.PaginationPtr = ptr
+}
+
+func (s *ContentFilter) SortFilter(ptr *SortFilter) {
+	if s.ParamItems != nil {
+		s.ParamItems.SortFilter = ptr
+	}
+}
+
+func (s *ContentFilter) ValueMask(val any) {
+	if s.ParamItems != nil {
+		byteVal, byteErr := json.Marshal(val)
+		if byteErr != nil {
+			return
+		}
+
+		s.ParamItems.ValueMask = byteVal
+	}
+}
+
+// ParamItems contentFilter
+type ParamItems struct {
+	Name       string            `json:"name"`
+	PkgPath    string            `json:"pkgPath"`
+	Items      map[string]string `json:"items"`
+	SortFilter *SortFilter       `json:"sortFilter"`
+	ValueMask  json.RawMessage   `json:"valueMask"`
+}
+
+// Decode 解析内容过滤值
+func (s *ParamItems) Decode(request *http.Request) bool {
+	s.Items = map[string]string{}
+	vals := request.URL.Query()
+	for k, v := range vals {
+		s.Items[k] = v[0]
+	}
+
+	return true
+}
+
+// Encode ParamItems
+func (s *ParamItems) Encode(vals url.Values) url.Values {
+	for k, v := range s.Items {
+		if v != "" {
+			vals.Set(k, fmt.Sprintf("%s", v))
+		}
+	}
+
+	return vals
+}
+
+func (s *ParamItems) IsEqual(key string) bool {
+	val, ok := s.Items[key]
+	if !ok {
+		return false
+	}
+
+	idx := strings.LastIndex(val, "|")
+	if idx == -1 {
+		return true
+	}
+	return val[idx+1:] == "="
+}
+
+func (s *ParamItems) GetEqual(key string) interface{} {
+	val, ok := s.Items[key]
+	if !ok {
+		return nil
+	}
+
+	return GetEqual(val)
+}
+
+func (s *ParamItems) IsNotEqual(key string) bool {
+	val, ok := s.Items[key]
+	if !ok {
+		return false
+	}
+
+	idx := strings.LastIndex(val, "|")
+	if idx == -1 {
+		return false
+	}
+	return val[idx+1:] == "!="
+}
+
+func (s *ParamItems) GetNotEqual(key string) interface{} {
+	val, ok := s.Items[key]
+	if !ok {
+		return nil
+	}
+
+	return GetNotEqual(val)
+}
+
+func (s *ParamItems) IsBelow(key string) bool {
+	val, ok := s.Items[key]
+	if !ok {
+		return false
+	}
+
+	idx := strings.LastIndex(val, "|")
+	if idx == -1 {
+		return true
+	}
+	return val[idx+1:] == "<"
+}
+
+func (s *ParamItems) GetBelow(key string) interface{} {
+	val, ok := s.Items[key]
+	if !ok {
+		return nil
+	}
+
+	return GetBelow(val)
+}
+
+func (s *ParamItems) IsAbove(key string) bool {
+	val, ok := s.Items[key]
+	if !ok {
+		return false
+	}
+
+	idx := strings.LastIndex(val, "|")
+	if idx == -1 {
+		return false
+	}
+	return val[idx+1:] == ">"
+}
+
+func (s *ParamItems) GetAbove(key string) interface{} {
+	val, ok := s.Items[key]
+	if !ok {
+		return nil
+	}
+
+	return GetAbove(val)
+}
+
+func (s *ParamItems) IsIn(key string) bool {
+	val, ok := s.Items[key]
+	if !ok {
+		return false
+	}
+
+	idx := strings.LastIndex(val, "|")
+	if idx == -1 {
+		return true
+	}
+	return val[idx+1:] == "in"
+}
+
+func (s *ParamItems) GetIn(key string) interface{} {
+	val, ok := s.Items[key]
+	if !ok {
+		return nil
+	}
+
+	return GetIn(val)
+}
+
+func (s *ParamItems) IsNotIn(key string) bool {
+	val, ok := s.Items[key]
+	if !ok {
+		return false
+	}
+
+	idx := strings.LastIndex(val, "|")
+	if idx == -1 {
+		return false
+	}
+	return val[idx+1:] == "!in"
+}
+
+func (s *ParamItems) GetNotIn(key string) interface{} {
+	val, ok := s.Items[key]
+	if !ok {
+		return nil
+	}
+
+	return GetNotIn(val)
+}
+
+func (s *ParamItems) IsLike(key string) bool {
+	val, ok := s.Items[key]
+	if !ok {
+		return false
+	}
+
+	idx := strings.LastIndex(val, "|")
+	if idx == -1 {
+		return false
+	}
+	return val[idx+1:] == "like"
+}
+
+func (s *ParamItems) GetLike(key string) interface{} {
+	val, ok := s.Items[key]
+	if !ok {
+		return nil
+	}
+
+	return GetLike(val)
+}
+
+func GetEqual(val string) interface{} {
+	idx := strings.LastIndex(val, "|")
+	if idx == -1 {
+		return UnmarshalString(val)
+	}
+	if val[idx+1:] != "=" {
+		return nil
+	}
+
+	return UnmarshalString(val[:idx])
+}
+
+func GetNotEqual(val string) interface{} {
+	idx := strings.LastIndex(val, "|")
+	if idx == -1 {
+		return nil
+	}
+	if val[idx+1:] != "!=" {
+		return nil
+	}
+
+	return UnmarshalString(val[:idx])
+}
+
+func GetBelow(val string) interface{} {
+	idx := strings.LastIndex(val, "|")
+	if idx == -1 {
+		return nil
+	}
+	if val[idx+1:] != "<" {
+		return nil
+	}
+
+	return UnmarshalString(val[:idx])
+}
+
+func GetAbove(val string) interface{} {
+	idx := strings.LastIndex(val, "|")
+	if idx == -1 {
+		return nil
+	}
+	if val[idx+1:] != ">" {
+		return nil
+	}
+
+	return UnmarshalString(val[:idx])
+}
+
+func GetIn(val string) interface{} {
+	idx := strings.LastIndex(val, "|")
+	if idx == -1 {
+		return nil
+	}
+	if val[idx+1:] != "in" {
+		return nil
+	}
+
+	return UnmarshalString(val[:idx])
+}
+
+func GetNotIn(val string) interface{} {
+	idx := strings.LastIndex(val, "|")
+	if idx == -1 {
+		return nil
+	}
+	if val[idx+1:] != "!in" {
+		return nil
+	}
+
+	return UnmarshalString(val[:idx])
+}
+
+func GetLike(val string) interface{} {
+	idx := strings.LastIndex(val, "|")
+	if idx == -1 {
+		return nil
+	}
+	if val[idx+1:] != "like" {
+		return nil
+	}
+
+	return val[:idx]
+}
