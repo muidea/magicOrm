2b1b733202dd4be2ceda0aa1710ef91add971a55
muidea <rangh@foxmail.com>
2023-12-29 09:03:17 +0800

    feat: 临时代码提交

--- model/const.go
+++ model/const.go
@@ -2,6 +2,71 @@ package model
 
 import "fmt"
 
+type ValueDeclare int
+
+const (
+	Customer ValueDeclare = iota
+	AutoIncrement
+	UUID
+	SnowFlake
+	DateTime
+)
+
+func (s ValueDeclare) String() string {
+	switch s {
+	case AutoIncrement:
+		return "autoIncrement"
+	case UUID:
+		return "uuid"
+	case SnowFlake:
+		return "snowFlake"
+	case DateTime:
+		return "dateTime"
+	default:
+		return "customer"
+	}
+}
+
+func (s ValueDeclare) IsCustomer() bool {
+	return s == Customer
+}
+
+func (s ValueDeclare) IsAutoIncrement() bool {
+	return s == AutoIncrement
+}
+
+func (s ValueDeclare) IsUUID() bool {
+	return s == UUID
+}
+
+func (s ValueDeclare) IsSnowFlake() bool {
+	return s == SnowFlake
+}
+
+func (s ValueDeclare) IsDaeTime() bool {
+	return s == DateTime
+}
+
+func IsCustomer(val ValueDeclare) bool {
+	return val == Customer
+}
+
+func IsAutoIncrement(val ValueDeclare) bool {
+	return val == AutoIncrement
+}
+
+func IsUUID(val ValueDeclare) bool {
+	return val == UUID
+}
+
+func IsSnowFlake(val ValueDeclare) bool {
+	return val == SnowFlake
+}
+
+func IsDateTime(val ValueDeclare) bool {
+	return val == DateTime
+}
+
 type TypeDeclare int
 
 // Define the Type enum
@@ -44,38 +109,67 @@ const (
 	TypeMapValue = 500
 )
 
-type ValueDeclare int
-
-const (
-	Customer ValueDeclare = iota
-	AutoIncrement
-	UUID
-	SnowFlake
-	DateTime
-)
-
-func (s ValueDeclare) String() string {
-	return fmt.Sprintf("%d", s)
+func (s TypeDeclare) String() string {
+	switch s {
+	case TypeBooleanValue:
+		return "bool"
+	case TypeBitValue:
+		return "int8"
+	case TypeSmallIntegerValue:
+		return "int16"
+	case TypeInteger32Value:
+		return "int32"
+	case TypeIntegerValue:
+		return "int"
+	case TypeBigIntegerValue:
+		return "int64"
+	case TypePositiveBitValue:
+		return "uint8"
+	case TypePositiveSmallIntegerValue:
+		return "uint16"
+	case TypePositiveInteger32Value:
+		return "uint32"
+	case TypePositiveIntegerValue:
+		return "uint"
+	case TypePositiveBigIntegerValue:
+		return "uint64"
+	case TypeFloatValue:
+		return "float32"
+	case TypeDoubleValue:
+		return "float64"
+	case TypeStringValue:
+		return "string"
+	case TypeDateTimeValue:
+		return "time"
+	case TypeStructValue:
+		return "struct"
+	case TypeSliceValue:
+		return "array"
+	case TypeMapValue:
+		return "map"
+	default:
+		return fmt.Sprintf("illegal type %d", s)
+	}
 }
 
-func IsCustomer(val ValueDeclare) bool {
-	return val == Customer
+func (s TypeDeclare) IsBasicType() bool {
+	return s < TypeStructValue
 }
 
-func IsAutoIncrement(val ValueDeclare) bool {
-	return val == AutoIncrement
+func (s TypeDeclare) IsDateTimeType() bool {
+	return s == TypeDateTimeValue
 }
 
-func IsUUID(val ValueDeclare) bool {
-	return val == UUID
+func (s TypeDeclare) IsStructType() bool {
+	return s == TypeStructValue
 }
 
-func IsSnowFlake(val ValueDeclare) bool {
-	return val == SnowFlake
+func (s TypeDeclare) IsSliceType() bool {
+	return s == TypeSliceValue
 }
 
-func IsDateTime(val ValueDeclare) bool {
-	return val == DateTime
+func (s TypeDeclare) IsMapType() bool {
+	return s == TypeMapValue
 }
 
 func IsBasicType(typeValue TypeDeclare) bool {
@@ -113,3 +207,16 @@ const (
 	FullView   = 1
 	LiteView   = 2
 )
+
+func (s ViewDeclare) String() string {
+	switch s {
+	case OriginView:
+		return "origin"
+	case FullView:
+		return "detail"
+	case LiteView:
+		return "lite"
+	default:
+		return "unknown"
+	}
+}
